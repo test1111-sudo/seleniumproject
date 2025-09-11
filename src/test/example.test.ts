904578932a0d6fca976a3af2b329b1dcf8e18a2b
test1111-sudo <t3stmyemail1111@gmail.com>
2025-09-11 11:13:53 +0400

    repo

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,6 +1,7 @@
 import { describe, afterEach, beforeEach, it } from "mocha";
 import { expect } from "chai";
 import { BaseTest } from "./base/baseTest";
+import { By } from "selenium-webdriver";
 
 
 describe("experiment suite", function () {
@@ -18,11 +19,13 @@ it("experiment testcase", async function () {
 await baseTest.homePage.actions.navigateToPage("https://automationexercise.com/");
 await baseTest.homePage.waiters.setImplicitWait(3000);
 await baseTest.homePage.actions.clickOnElement(baseTest.homePage.signupLoginBtn);
-
-const urlIsCorrect = await baseTest.signUpPage.assertions.verifyPageUrl(
-"https://automationexercise.com/login"
-);
-
+const urlIsCorrect = await baseTest.signUpPage.assertions.verifyPageUrl("https://automationexercise.com/login");
 expect(urlIsCorrect).to.be.true;
+await baseTest.signUpPage.assertions.verifyElementIsVisible(By.xpath("//h2[text()='Login to your account']"));
+
+
+
+
+
 });
 });
